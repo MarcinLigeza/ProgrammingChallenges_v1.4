7e2e3cba309c8dc58c24249e2a88edce5e91d4ca
unknown <marcin.ligeza2@gmail.com>
2019-02-03 22:04:54 +0100

    05_FizzBuzz - done

---
+++ 0X/05_FizzBuzz/main.cpp
@@ -0,0 +1,26 @@
+//#################################################################
+// Programming challenges v1.4 - 05 Fizz Buzz
+// Marcin Ligęza 2019
+//#################################################################
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "\nProgramming challenges v1.4 - 05 Fizz Buzz\n" << endl;
+
+    for (int i = 1; i < 100; i++)
+    {
+        string output{};
+        if(i % 3 == 0)
+            output += "Fizz";
+        if(i % 5 == 0)
+            output += "Buzz";
+        else if (i % 3)
+            output += to_string(i);
+        cout << output << " ,";
+    }
+}
